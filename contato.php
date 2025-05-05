720d23f7113f28b0d1025fcd4a21eebeafff3f73
Dhiogo-Antonio <dhiogo.marestoni@gmail.com>
2025-05-05 14:56:48 -0300

    php

---
+++ contato.php
@@ -0,0 +1,7 @@
+<?php
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$assunto = $_POST['assunto'];
+
+echo "O E-mail de '$nome' foi enviado com sucesso";
+?>
---
+++ dados.php
@@ -0,0 +1,8 @@
+<?php
+$nome = $_POST["nome"];
+$sobrenome = $_POST["sobrenome"];
+$email = $_POST["email"];
+$data_nasc = $_POST["datanasc"];
+
+echo "O participante '$nome' foi cadastrado com sucesso."; 
+?>
